9ef78769b707a145f1a0bed04ad2f662ae045495
dabweezy <168998991+dabweezy@users.noreply.github.com>
2025-09-26 20:10:51 +0100

    Add external image domains to next.config.js

--- bespoke-wall-printing/next.config.js
+++ bespoke-wall-printing/next.config.js
@@ -10,6 +10,28 @@ const nextConfig = {
     // your project has type errors.
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
